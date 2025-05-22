33f9c0f963bd208cd6c08cd56f4c30074785b30a
Matthew Abbott <ttobbatam@gmail.com>
2025-05-22 04:49:24 +0000

    Add explicit timeout to client requests

--- src/main.tsx
+++ src/main.tsx
@@ -3,24 +3,34 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 
-import { ApolloClient, InMemoryCache, HttpLink, split } from '@apollo/client';
+import { ApolloClient, InMemoryCache, HttpLink, split, from, ApolloLink } from '@apollo/client';
 import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
 import { createClient } from 'graphql-ws';
 import { getMainDefinition } from '@apollo/client/utilities';
 import { ApolloProvider } from '@apollo/client';
+import TimeoutLink from 'apollo-link-timeout';
 
 const httpLink = new HttpLink({
   uri: 'http://localhost:4000/dice/graphql',
 });
 
+// Timeout link - e.g., 5 seconds (5000 milliseconds)
+const timeoutLink = new TimeoutLink(5000);
+
+// Chain timeoutLink and httpLink for HTTP requests
+// timeoutLink is cast because its type definitions (Observable<unknown>)
+// are not directly compatible with Apollo Client 3's expected Link types (Observable<FetchResult>).
+// so not ideal... but works for now
+// TODO: update to Apollo Client 4
+const httpLinkWithTimeout = from([timeoutLink as ApolloLink, httpLink]);
+
 const wsLink = new GraphQLWsLink(
   createClient({
     url: 'ws://localhost:4000/dice/graphql',
-    protocol: 'graphql-transport-ws',
   })
 );
 
-// Queries/mutations to HTTP endpoint, subscriptions to WebSocket
+// Queries/mutations to HTTP endpoint (with timeout), subscriptions to WebSocket
 const splitLink = split(
   ({ query }) => {
     const definition = getMainDefinition(query);
@@ -30,7 +40,7 @@ const splitLink = split(
     );
   },
   wsLink,
-  httpLink
+  httpLinkWithTimeout
 );
 
 const client = new ApolloClient({
