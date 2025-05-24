c1f8910e0a54c378a3d7c3190faf8fc4c20f1353
Matthew Abbott <ttobbatam@gmail.com>
2025-05-24 06:00:47 +0000

    Set up physics inteface, types, and base dicephysics utils

---
+++ src/physics/index.ts
@@ -0,0 +1,7 @@
+// Physics module exports
+// This will be populated as we implement each component
+
+export * from './types';
+// export * from './DiceManager';
+// export * from './DiceObject';
+// export * from './dice'; 
