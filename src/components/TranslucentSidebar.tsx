dbf4e121d405f7c619bf0b4b5fb060932f61b5ea
Matthew Abbott <ttobbatam@gmail.com>
2025-05-27 17:27:12 +0000

    Make translucent sub-elements also translucent

--- src/components/TranslucentSidebar.tsx
+++ src/components/TranslucentSidebar.tsx
@@ -19,15 +19,36 @@ const TranslucentSidebar: React.FC<TranslucentSidebarProps> = ({
     return (
         <div
             className={`
-        h-full 
-        bg-brand-background/80 
-        backdrop-blur-sm 
-        ${side === 'left' ? 'border-r' : 'border-l'} 
-        border-white/10
-        ${className}
-      `}
+                h-full 
+                bg-brand-background/80 
+                backdrop-blur-sm 
+                ${side === 'left' ? 'border-r' : 'border-l'} 
+                border-white/10
+                ${className}
+            `}
         >
-            {children}
+            <div className="h-full 
+                [&_.card]:bg-brand-surface/70 
+                [&_.card]:backdrop-blur-sm 
+                [&_.card]:border-white/10
+                [&_button]:bg-opacity-80 
+                [&_button]:backdrop-blur-sm
+                [&_input]:bg-opacity-80 
+                [&_input]:backdrop-blur-sm
+                [&_.bg-brand-surface]:bg-brand-surface/60
+                [&_.bg-brand-background]:bg-brand-background/70
+                [&_.border-brand-surface]:border-white/10
+                [&_.text-brand-text]:text-white/90
+                [&_.text-brand-text-muted]:text-white/70
+                [&_.bg-orange-900\/20]:bg-orange-900/30
+                [&_.bg-blue-600]:bg-blue-600/80
+                [&_.bg-green-600]:bg-green-600/80
+                [&_.bg-red-600]:bg-red-600/80
+                [&_.bg-gray-800]:bg-gray-800/80
+                [&_.bg-white]:bg-white/80
+            ">
+                {children}
+            </div>
         </div>
     );
 };
